6b82407395d4b7fedf32f90e881433b1c78e1578
Fred Kufner <59782695+waymorphood@users.noreply.github.com>
2021-10-30 14:52:18 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <Hooper>
+      <Slide class="slide is-1">1</Slide>
+      <Slide class="slide is-2">2</Slide>
+      <Slide class="slide is-3">3</Slide>
+      <Slide class="slide is-4">4</Slide>
+      <Slide class="slide is-5">5</Slide>
+    </Hooper>
+  </div>
+</template>
+
+<script>
+import { Slide, Hooper } from 'hooper'
+
+export default {
+  components: {
+    Slide,
+    Hooper
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.hooper {
+  height: 100vh;
+}
+
+.slide {
+  font-size: 100px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.is-1 {
+  background: red;
+}
+
+.is-2 {
+  background: purple;
+}
+
+.is-3 {
+  background: blue;
+}
+
+.is-4 {
+  background: green;
+}
+
+.is-5 {
+  background: gray;
+}
+</style>
